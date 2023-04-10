003dedcd7ac166051b137d9ffc800a18630ea6d3
odc <Hassan-Daboos>
2023-04-10 14:55:38 +0200

    fire

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:clinicdoctor/viewmodel/cubit/layout_cubit/layout_cubit.dart';
 import 'package:clinicdoctor/viewmodel/database/CacheHelper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -16,6 +17,7 @@ import 'view/screens/layouthome/profileScreens/MedicalHistoryScreen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await CacheHelper.init();
   // await DioHelper.init();
   Bloc.observer = MyBlocObserver();
