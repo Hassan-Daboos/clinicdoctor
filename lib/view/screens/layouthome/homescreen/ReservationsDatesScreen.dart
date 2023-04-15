c49da1b52805e22561a8ab2ba7d23be933cae01a
Hassan Daboos <62604224+Hassan-Daboos@users.noreply.github.com>
2023-04-16 01:10:09 +0200

    add in home

---
+++ lib/view/screens/layouthome/homescreen/ReservationsDatesScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class ReservationsDatesScreen extends StatefulWidget {
+  const ReservationsDatesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ReservationsDatesScreen> createState() => _ReservationsDatesScreenState();
+}
+
+class _ReservationsDatesScreenState extends State<ReservationsDatesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.grey.shade100,
+
+
+
+
+    );
+  }
+}
